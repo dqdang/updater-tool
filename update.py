2a194d4ed70e6c6422cd963487612a0d85ef0391
Derek Dang <dqdang17@gmail.com>
2020-06-18 19:44:05 -0700

    Add Windows support and exception handling

--- update.py
+++ update.py
@@ -1,4 +1,6 @@
 import os
+import platform
+import shlex
 import subprocess
 import sys
 
@@ -6,13 +8,21 @@ def update(path):
     for root, directory, files in os.walk(path):
         for name in files:
             name = os.path.join(root, name)
-            if "dropbot" in name:
-                continue
+            # if "dropbot" in name:
+            #     continue
             if "requirements.txt" in name:
                 os.chdir(os.path.dirname(name))
-                cmd = "pip install -r requirements.txt"
+                if platform.system() == "Windows":
+                    cmd = "python -m pip install -r requirements.txt"
+                else:
+                    cmd = "pip install -r requirements.txt"
                 print(os.path.dirname(name))
-                print(subprocess.check_output(["pip", "install", "-r", "requirements.txt"]).decode())
+                try:
+                    print(subprocess.check_output(shlex.split(cmd)).decode())
+                except Exception as e:
+                    print("Failed to install requirements for {}".format(root))
+                finally:
+                    continue
 
 def main():
     update(sys.argv[1])
